5fc2476b220c5eaa1ce871c3d457e568a3254d7a
Arifin-MRX <arifinmrx@gmail.com>
2023-10-30 12:33:57 +0700

    [Bab5] Navigation

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
-// import * as React from 'react';
-// import {Discover} from './src/screens';
-// export default function App() {
-//   return <Discover />;
-// }
 import * as React from 'react';
-import {Bookmark} from './src/screens';
+import { NavigationContainer } from '@react-navigation/native';
+import Router from './src/navigation/Router';
 export default function App() {
-  return <Bookmark />;
+  return (
+    <NavigationContainer>
+     <Router/>
+    </NavigationContainer>
+  );
 }
-// import * as React from 'react';
-// import {Profile} from './src/screens';
-// export default function App() {
-//   return <Profile />;
-// }
